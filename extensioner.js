66d64363917911cf5f6880c7c2076fda83b6e994
Masoud <MasoudRahmani@users.noreply.github.com>
2018-06-08 23:49:21 +0430

    Update extensioner.js

--- extensioner.js
+++ extensioner.js
@@ -3,6 +3,12 @@ var extensions;
 var options;
 var extensionOrder;
 
+var pop_body=document.getElementById("pop_body");
+if(pop_body !=null) pop_body.addEventListener("onload",setupPopupPage());
+
+var optionbody=document.getElementById("option_body");
+if(optionbody !=null) optionbody.addEventListener("onload",setupOptionsPage());
+
 /* Return groups from localStorage or [] */
 function loadGroups() {
   var groupStorage = localStorage.extensionerGroups;
@@ -542,3 +548,5 @@ function setupPopupPage() {
   chrome.management.getAll(setupGroups);
   $('#setup_link a').attr('href', chrome.extension.getURL('options.html'));
 }
+
+
